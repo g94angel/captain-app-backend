92166ba0ce99c62f5f3196ecfa88656b75bb30e6
Angel Giron <angel.c.giron1@gmail.com>
2022-10-26 19:17:20 -0700

    reorganized code in server file

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const path = require('path');
-const cors = require('cors');
 const express = require('express');
-require('dotenv').config();
+const app = express();
 const mongoose = require('mongoose');
 // const uuidv4 = require('uuid/v4');
 // const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
+const cors = require('cors');
 const controller = require('./controllers/claimController');
 // const imageController = require('./controllers/imageController');
-const imageRoutes = require('./routes/image');
 
-const app = express();
-const PORT = process.env.PORT || 5000;
 // middleware & static files
-app.use(cors());
 app.use(express.static(path.join(__dirname + '/public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 // using this to store images
 // const multer = require('multer');
 // const Image = require('./models/image');
+
+const PORT = process.env.PORT || 5000;
 
 // connect to mongoDB via dbURI string
 mongoose
