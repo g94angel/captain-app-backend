79ab69f5eb5a075bcd5d2ac4fa06fd6e7034a3cd
Angel Giron <angel.c.giron1@gmail.com>
2023-01-20 17:37:46 -0800

    added backend url to server file

--- server.js
+++ server.js
@@ -67,117 +67,141 @@ mongoose
   .catch((err) => console.log(err));
 
 // ROUTES TO HANDLE IMAGE UPLOADS
-app.post('/upload', upload, async (req, res) => {
-  // console.log('req.body', req.body); // other data
-  // console.log('req.file', req.file); // file upload data
+app.post(
+  'https://claimform-api.onrender.com/upload',
+  upload,
+  async (req, res) => {
+    // console.log('req.body', req.body); // other data
+    // console.log('req.file', req.file); // file upload data
 
-  // resize and beautify image we want to send to S3
-  // const buffer = await sharp(req.file.buffer);
-  // .resize({ height: 1920, width: 1080, fit: 'contain' })
-  // .toBuffer();
+    // resize and beautify image we want to send to S3
+    // const buffer = await sharp(req.file.buffer);
+    // .resize({ height: 1920, width: 1080, fit: 'contain' })
+    // .toBuffer();
 
-  const imageName = randomImageName();
-  console.log(req.file);
-  const params = {
-    Bucket: bucketName,
-    Key: imageName,
-    Body: req.file.buffer,
-    ContentType: req.file.mimetype,
-  };
-  const command = new PutObjectCommand(params);
-
-  await s3.send(command);
-  res.send(imageName);
-});
-
-app.post('/uploads', multipleUploads, async (req, res) => {
-  let urls = '';
-  const params = req.files.map((file) => {
-    const randNum = randomImageName();
-    urls += `${randNum},`;
-    return {
-      Bucket: bucketName,
-      Key: randNum,
-      Body: file.buffer,
-      ContentType: file.mimetype,
-    };
-  });
-
-  await Promise.all(
-    params.map(async (param) => await s3.send(new PutObjectCommand(param)))
-  );
-
-  res.send({
-    data: req.files,
-    msg: 'Successfully uploaded ' + req.files.length + ' files!',
-    urls: urls,
-  });
-});
-
-app.get('/getclaims', controller.getClaims, async (req, res) => {
-  for (const claim of res.locals.claims) {
-    // if no image, then continue to next claim
-    if (!claim.imageName) {
-      // console.log('no image');
-      continue;
-    }
-    // if there are multiple
-    else if (claim.imageName.includes(',')) {
-      let returnedURLs = '';
-      const newArray = claim.imageName.split(',');
-      for (let i = 0; i < newArray.length; i++) {
-        const getObjectParams = {
-          Bucket: bucketName,
-          Key: newArray[i],
-        };
-        const command = new GetObjectCommand(getObjectParams);
-
-        const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
-
-        returnedURLs += `${url},`;
-      }
-      claim.url = returnedURLs;
-    }
-    // if there is one
-    else if (claim.imageName) {
-      const getObjectParams = {
-        Bucket: bucketName,
-        Key: claim.imageName,
-      };
-      const command = new GetObjectCommand(getObjectParams);
-      const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
-
-      claim.url = url;
-    }
-  }
-
-  res.status(200).json(res.locals.claims);
-});
-
-app.post('/newclaim', controller.newClaim, (req, res) => {
-  res.status(200).json(res.locals.newClaim);
-});
-
-app.put('/updateclaim', controller.updateClaim, (req, res) => {
-  res.status(200).json(res.locals.updatedClaim);
-});
-
-app.delete('/deleteclaim/:id', controller.deleteClaim, async (req, res) => {
-  // if the deleted claim has an image this code also deletes the image from S3
-
-  const { deletedClaim } = res.locals;
-  console.log('inside server deleted claim', deletedClaim);
-  if (deletedClaim.imageName) {
-    const { imageName } = deletedClaim;
+    const imageName = randomImageName();
+    console.log(req.file);
     const params = {
       Bucket: bucketName,
       Key: imageName,
+      Body: req.file.buffer,
+      ContentType: req.file.mimetype,
     };
-    const command = new DeleteObjectCommand(params);
+    const command = new PutObjectCommand(params);
+
     await s3.send(command);
+    res.send(imageName);
   }
-  res.status(200).json(res.locals.deletedClaim);
-});
+);
+
+app.post(
+  'https://claimform-api.onrender.com/uploads',
+  multipleUploads,
+  async (req, res) => {
+    let urls = '';
+    const params = req.files.map((file) => {
+      const randNum = randomImageName();
+      urls += `${randNum},`;
+      return {
+        Bucket: bucketName,
+        Key: randNum,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+      };
+    });
+
+    await Promise.all(
+      params.map(async (param) => await s3.send(new PutObjectCommand(param)))
+    );
+
+    res.send({
+      data: req.files,
+      msg: 'Successfully uploaded ' + req.files.length + ' files!',
+      urls: urls,
+    });
+  }
+);
+
+app.get(
+  'https://claimform-api.onrender.com/getclaims',
+  controller.getClaims,
+  async (req, res) => {
+    for (const claim of res.locals.claims) {
+      // if no image, then continue to next claim
+      if (!claim.imageName) {
+        // console.log('no image');
+        continue;
+      }
+      // if there are multiple
+      else if (claim.imageName.includes(',')) {
+        let returnedURLs = '';
+        const newArray = claim.imageName.split(',');
+        for (let i = 0; i < newArray.length; i++) {
+          const getObjectParams = {
+            Bucket: bucketName,
+            Key: newArray[i],
+          };
+          const command = new GetObjectCommand(getObjectParams);
+
+          const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+
+          returnedURLs += `${url},`;
+        }
+        claim.url = returnedURLs;
+      }
+      // if there is one
+      else if (claim.imageName) {
+        const getObjectParams = {
+          Bucket: bucketName,
+          Key: claim.imageName,
+        };
+        const command = new GetObjectCommand(getObjectParams);
+        const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+
+        claim.url = url;
+      }
+    }
+
+    res.status(200).json(res.locals.claims);
+  }
+);
+
+app.post(
+  'https://claimform-api.onrender.com/newclaim',
+  controller.newClaim,
+  (req, res) => {
+    res.status(200).json(res.locals.newClaim);
+  }
+);
+
+app.put(
+  'https://claimform-api.onrender.com/updateclaim',
+  controller.updateClaim,
+  (req, res) => {
+    res.status(200).json(res.locals.updatedClaim);
+  }
+);
+
+app.delete(
+  'https://claimform-api.onrender.com/deleteclaim/:id',
+  controller.deleteClaim,
+  async (req, res) => {
+    // if the deleted claim has an image this code also deletes the image from S3
+
+    const { deletedClaim } = res.locals;
+    console.log('inside server deleted claim', deletedClaim);
+    if (deletedClaim.imageName) {
+      const { imageName } = deletedClaim;
+      const params = {
+        Bucket: bucketName,
+        Key: imageName,
+      };
+      const command = new DeleteObjectCommand(params);
+      await s3.send(command);
+    }
+    res.status(200).json(res.locals.deletedClaim);
+  }
+);
 
 // Unknown route handler
 app.use((req, res) => res.sendStatus(404));
